fc9fd531dfc7d0fc8732d559de3dc344fe8efba1
Dominik Kubis <kubis.dominik1@gmail.com>
2017-05-02 23:55:51 +0200

    add Phaser to project

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
+const phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.js');
+const p2 = path.join(phaserModule, 'build/custom/p2.js');
+const VENDOR_LIBS = ['pixi', 'p2', 'phaser'];
+
 module.exports = {
   entry: {
     bundle: './src/index.js',
+    vendor: VENDOR_LIBS,
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -17,6 +24,18 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
       },
+      {
+        use: ['expose-loader?PIXI'],
+        test: /pixi\.js/,
+      },
+      {
+        use: ['expose-loader?Phaser'],
+        test: /phaser-split\.js$/,
+      },
+      {
+        use: ['expose-loader?p2'],
+        test: /p2\.js/,
+      },
     ],
   },
   plugins: [
@@ -26,5 +45,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest'],
+    }),
   ],
+  resolve: {
+    alias: {
+      phaser,
+      pixi,
+      p2,
+    },
+  },
 };
