7cd7054aa10e72d4d3f6835bab774e6abc83e436
Dominik Kubis <kubis.dominik1@gmail.com>
2017-05-06 19:33:38 +0200

    load images to game

--- src/states/Preload.js
+++ src/states/Preload.js
@@ -1,6 +1,14 @@
 import Phaser from 'phaser';
+import config from '../config';
 
 export default class extends Phaser.State {
+
+  preload() {
+    const imagesPath = '../assets/images';
+
+    this.load.spritesheet('block', `${imagesPath}/blocks_spritesheet.png`, config.BLOCK_SIDE_LENGTH, config.BLOCK_SIDE_LENGTH);
+    this.load.image('background', `${imagesPath}/background.png`);
+  }
 
   create() {
     this.state.start('Play');
