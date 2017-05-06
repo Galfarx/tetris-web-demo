8de3d6dbd88fdcfaa402f4864885c03473e94eb9
Dominik Kubis <kubis.dominik1@gmail.com>
2017-05-06 18:52:50 +0200

    create empty board filled with nulls

--- src/states/Play.js
+++ src/states/Play.js
@@ -1,11 +1,19 @@
 import Phaser from 'phaser';
 
+import config from '../config';
+
 export default class extends Phaser.State {
   init() {
     this.cursors = this.game.input.keyboard.createCursorKeys();
   }
 
   create() {
-    console.log('PhaserOn');
+    this.board = new Array(config.BOARD_HEIGHT);
+    for (let i = 0; i < config.BOARD_HEIGHT; i++) {
+      this.board[i] = new Array(this.BOARD_WIDTH);
+      for (let j = 0; j < config.BOARD_WIDTH; j++) {
+        this.board[i][j] = null;
+      }
+    }
   }
 }
