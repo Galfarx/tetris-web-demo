02bd604204e670453b022181addeec6ffa5793d4
Dominik Kubis <kubis.dominik1@gmail.com>
2017-05-07 19:14:13 +0200

    move shape down with speed 1 row per second

--- src/states/Play.js
+++ src/states/Play.js
@@ -20,16 +20,24 @@ export default class extends Phaser.State {
       }
     }
 
+    this.turnLength = 60;
+    this.turnCounter = 0;
+
     this.activeShape = new Shape();
     this.activeShape.createRandomShape();
     this.activeShape.activate();
   }
 
   update() {
-    if (this.activeShape !== null && this.activeShape.canMoveShape(config.MOVE_DOWN)) {
-      this.activeShape.moveShape(config.MOVE_DOWN);
+    if (this.turnCounter >= this.turnLength) {
+      if (this.activeShape !== null && this.activeShape.canMoveShape(config.MOVE_DOWN)) {
+        this.activeShape.moveShape(config.MOVE_DOWN);
+      } else {
+        this.activeShape.placeShapeInBoard();
+      }
+      this.turnCounter = 0;
     } else {
-      this.activeShape.placeShapeInBoard();
+      this.turnCounter++;
     }
   }
 }
